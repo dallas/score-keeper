1feea8417d6497c1d02581c43d8b895a260829a8
Dallas Reedy <code@dallasreedy.com>
2011-02-17 22:21:20 -0800

    initial commit of rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreKeeper::Application.config.secret_token = '191a98126ffe2679444ee0fbe269ce1f05b493c3405f67e73e16925f44609331197c7533e407840b5a8e228e9a737cc66965dc56d371e26c9e90cac9943be033'
