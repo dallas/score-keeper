abcb4b959c4e1db01594502634823eb9b093e303
Dallas Reedy <code@dallasreedy.com>
2011-02-17 22:56:28 -0800

    added super simple Gravatar images support

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,6 @@
 module PlayersHelper
+  def avatar_url(player, size = 100)
+    gravatar_id = Digest::MD5::hexdigest(player.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
