81e9283207387ff14c644b3b3a70a9e93474792f
Dallas Reedy <code@dallasreedy.com>
2011-03-28 22:29:14 -0700

    fixing up the GamePlayersController#update action to respond specifically to a .json request as well as to an .html request (if JS fails); updated application.js

--- app/controllers/game_players_controller.rb
+++ app/controllers/game_players_controller.rb
@@ -1,10 +1,11 @@
 class GamePlayersController < ApplicationController
   def update
-    @game_player = GamePlayer.find(params[:id], :include => {:game_play => :game})
-    @game = @game_player.game_play.game
-    @success = @game_player.update_attributes(params[:game_player])
+    game_player = GamePlayer.find(params[:id])
+    game_player.update_attributes(params[:game_player])
+    json_hash = { :id => dom_id(game_player), :score => game_player.score }
     respond_to do |format|
-      format.js
+      format.html { redirect_to :back }
+      format.json { render :json => json_hash }
     end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,70 @@
-function updatePlayerScore(selector, score) {
-  $(selector + ' .score').html(score);
-  $(selector + ' .update-score :text').val(0);
-  resortPlayers();
+var Game = {};
+
+Game.playerList = function() {
+  Game._playerList = Game._playerList || $('#players');
+  return Game._playerList;
 }
 
-function resortPlayers() {
-  var playerList, players, sortOrder;
-  playerList  = $('#players');
-  players     = getPlayers(playerList);
-  sortOrder   = playerList.attr('data-sort-order');
-  players.sort(function(a, b) {
-    var A, B;
-    A = parseInt($(a).find('.score').text());
-    B = parseInt($(b).find('.score').text());
-    if (A < B) return sortOrder == 'asc' ? -1 : 1;
-    if (A > B) return sortOrder == 'asc' ? 1 : -1;
-    return 0;
-  });
-  $.each(players, function(_, player) { playerList.append(player); });
-  updatePlayers(playerList);
+Game.players = function() {
+  Game._players = Game._players || Game.playerList().find('.player');
+  return Game._players;
+}
+
+Game.changeScore = function(player, score) {
+  Game.playerScore(player, score);
+  Game.sortPlayers();
+}
+
+Game.playerScore = function(player, score) {
+  var playerScore = $(player).find('.score');
+  if (score === undefined) return parseInt(playerScore.text());
+  else {
+    playerScore.html(score);
+    $(player).find('.update-score :text').val('');
+  }
+}
+
+Game.sortOrder = function() {
+  Game._sortOrder = Game._sortOrder || Game.playerList().attr('data-sort-order');
+  return Game._sortOrder;
+}
+
+Game.sortFunction = function() { return Game[Game.sortOrder() + 'Sorter']; }
+
+Game.ascSorter = function(a, b) {
+  var A = Game.playerScore(a),
+      B = Game.playerScore(b);
+  return A == B ? 0 : A < B ? -1 : 1;
+}
+
+Game.descSorter = function(a, b) {
+  var A = Game.playerScore(a),
+      B = Game.playerScore(b);
+  return A == B ? 0 : A < B ? 1 : -1;
+}
+
+Game.sortPlayers = function() {
+  var list = Game.playerList();
+  $.each(Game.players().sort(Game.sortFunction()), function(_,p) { list.append(p); });
+  Game.updatePlayers();
+}
+
+Game.updatePlayers = function() {
+  var players = Game.players();
+  Game.playerList().find('.grid_4').removeClass('grid_4').addClass('grid_3');
+  players.eq(0).removeClass('grid_3').addClass('grid_4');
   $('#graph, #graph + br.clear').insertAfter($('#players .player.grid_4'));
 }
 
-function updatePlayers(playerList) {
-  players = getPlayers(playerList);
-  playerList.find('.grid_4').removeClass('grid_4').addClass('grid_3');
-  players.eq(0).removeClass('grid_3').addClass('grid_4');
+Game.scoreUpdated = function(e, data) {
+  Game.changeScore($(e.target).closest('.player'), data.score);
 }
 
-function getPlayers(playerList) { return playerList.children('.player'); }
+$(function() {
+  $('.player form').bind('ajax:success', Game.scoreUpdated);
+  $('.player :text').blur(function(e) { $(e.target).closest('form').submit(); });
+
+  $('[data-game-names]').each(function(_,field) {
+    $(field).autocomplete($(field).attr('data-game-names').split(';:'));
+  });
+});
