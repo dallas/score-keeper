9a8ec1bd4aebb0013f73b0c51af42e0e28b9b364
Dallas Reedy <code@dallasreedy.com>
2011-05-03 21:02:19 -0700

    added testing gems and created tests for the Game model

--- Gemfile
+++ Gemfile
@@ -4,3 +4,9 @@ gem 'rails', '3.0.4'
 gem 'sqlite3'
 gem 'jquery-rails'
 gem 'haml'
+
+group :test do
+  # gem 'redgreen' # "`require': no such file to load -- test/unit/ui/console/testrunner"
+  gem 'shoulda'
+  gem 'mocha'
+end
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -1,8 +1,51 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  should have_many(:game_plays).dependent(:destroy)
+
+  should allow_mass_assignment_of(:name)
+  should allow_mass_assignment_of(:scoring_strategy)
+  should_not allow_mass_assignment_of(:id)
+  should_not allow_mass_assignment_of(:created_at)
+  should_not allow_mass_assignment_of(:updated_at)
+
+  should validate_presence_of(:name)
+  should validate_presence_of(:scoring_strategy)
+
+  context 'public instance method' do
+    subject { games(:uno) }
+
+    context :as_json do
+      setup do
+        @json = {
+          'id' => Fixtures.identify(:uno),
+          'name' => 'UNO',
+          'scoring_strategy' => 'low'
+        }
+      end
+
+      should 'only allow id, name, and scoring_strategy by default' do
+        assert_equal @json, subject.as_json
+      end
+
+      should 'not allow overwriting the :only option' do
+        assert_equal @json, subject.as_json(:only => [:foo, :bar, :baz])
+      end
+
+      should 'allow other options to be passed along' do
+        json = @json.merge(:scoring_strategy_as_sql_order => 'asc')
+        assert_equal json, subject.as_json(:methods => :scoring_strategy_as_sql_order)
+      end
+    end
+
+    context :scoring_strategy_as_sql_order do
+      should 'return "asc" when scoring strategy is "low"' do
+        assert_equal 'asc', subject.scoring_strategy_as_sql_order
+      end
+
+      should 'return "desc" when scoring strategy is "high"' do
+        assert_equal 'desc', games(:canasta).scoring_strategy_as_sql_order
+      end
+    end
   end
 end
